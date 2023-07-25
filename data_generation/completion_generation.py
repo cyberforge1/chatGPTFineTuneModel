43009ba543b8bf41792ac35ce49f083a981e29dc
cyber_forge <cyberforge1@gmail.com>
2023-07-26 08:41:33 +1000

    second commit

--- data_generation/completion_generation.py
+++ data_generation/completion_generation.py
@@ -13,7 +13,7 @@ def chat_with_gpt3(prompt):
     try:
         response = openai.Completion.create(
             engine="text-davinci-002",  # The GPT-3.5 chat model
-            prompt=prompt,
+            prompt= prompt + " with the response being provided in a poetic style",  # Add your string to the prompt
             max_tokens=150  # Adjust this value to control the response length
         )
         print(response.choices[0].text.strip())
@@ -22,15 +22,15 @@ def chat_with_gpt3(prompt):
         return str(e)
 
 
-response = chat_with_gpt3('tell me about dams')
-
-
 def process_excel_file(file_name):
     current_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(current_directory, file_name)
     
     workbook = openpyxl.load_workbook(file_path)
     sheet = workbook.active
+    
+    # Add 'Completions' to the first row of the second column
+    sheet.cell(row=1, column=2, value='Completions')
 
     for idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, max_col=1, values_only=True), start=2):
         prompt = row[0]
